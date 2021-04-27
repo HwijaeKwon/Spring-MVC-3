4d0f42af280c1f67be382ac92b0c7666d44031c6
hwijae_kwon <hwijae_kwon@tmax.co.kr>
2021-04-28 02:22:21 +0900

    [feat] RedirectAttribute를 이용하여 redirect 성능 개선
    - RedirectAttribute를 이용하여 redirect 성능을 개선
    - redirect 경로 인코딩
    - 저장 결과를 query parameter에 추가 -> item.html에서 사용

--- src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
@@ -82,12 +82,22 @@ public class BasicItemController {
         return "basic/item";
     }
 
-    @PostMapping("/add")
+    //@PostMapping("/add")
     public String addItemV5(Item item) {
         itemRepository.save(item);
         //PRG(Post Redirect Get Pattern)
         //아래 방식은 url encoding이 안된다
         return "redirect:/basic/items/" + item.getId();
+    }
+
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes) {
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        //status는 query parameter로 들어간다
+        //url encoding도 수행한다
+        return "redirect:/basic/items/{itemId}";
     }
 
     @GetMapping("/{itemId}/edit")
