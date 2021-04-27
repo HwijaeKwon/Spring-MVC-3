7f855a03d6f3b2180784f9876335b2dbd84c6b5c
hwijae_kwon <hwijae_kwon@tmax.co.kr>
2021-04-28 02:10:59 +0900

    [feat] item 수정 기능 추가
    - Controller에 item 수정을 위한 함수 추가
    - thymeleaf를 이용하여 item 수정을 위한 html 작성

--- src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
@@ -3,6 +3,7 @@ package hello.itemservice.web.item.basic;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -78,6 +79,27 @@ public class BasicItemController {
     public String addItemV4(Item item) {
         itemRepository.save(item);
         return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String addItemV5(Item item) {
+        itemRepository.save(item);
+        //PRG(Post Redirect Get Pattern)
+        //아래 방식은 url encoding이 안된다
+        return "redirect:/basic/items/" + item.getId();
+    }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}";
     }
 
     /**
